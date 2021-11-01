068d99ae9bdd12d229d129ed26df2a1a4f166b76
Editha Trinitaria <edithtrinitaria@gmail.com>
2021-11-01 10:53:36 +0100

    added png files in assets folder

--- Slutprojekt/main.js
+++ Slutprojekt/main.js
@@ -1,13 +1,14 @@
 const starWarsCharacterList = document.querySelector('ul');
 fetch('https://swapi.dev/api/people', {mode:'cors'}) 
     .then(function(response) {   
-    return response.json(); 
+        return response.json(); 
     })
     .then(function(json) {
-    let people = json.results; 
-    for(p of people) {
-        let listItem = document.createElement('li'); 
-        listItem.innerHTML = '<p>' + p.name + '</p>'; 
-        starWarsCharacterList.appendChild(listItem);  
-    }
+        let people = json.results; 
+        for(p of people) {
+            let listItem = document.createElement('li');
+            listItem.className = "starwars-character"
+            listItem.innerHTML = '<a>' + p.name + '</a>';
+            starWarsCharacterList.appendChild(listItem);  
+        }
 });
